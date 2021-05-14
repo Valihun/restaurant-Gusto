7bcf49e218188276392d4ed6d5582fa7e116f34e
Valihun <valihunvv@krok.edu.ua>
2021-05-14 16:17:50 +0300

    change

--- gusto/settings.py
+++ gusto/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 DB_NAME = os.environ.get('DB_NAME')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
@@ -23,6 +19,7 @@ DB_USER = os.environ.get('DB_USER')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -50,7 +47,7 @@ INSTALLED_APPS = [
     'main_gusto',
     'menu_gusto',
     'user_messages',
-    'accounts',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -86,16 +83,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'gusto.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -106,6 +93,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
 
 import dj_database_url
 db = dj_database_url.config()
@@ -149,6 +137,7 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
